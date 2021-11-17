c04b260d1df88f0fcb6b4e3ea81dae4d14dea0ca
gggoon <dlrkdrns123@naver.com>
2021-11-17 18:43:20 +0900

    [01:main]create ReServerProgram Project

---
+++ src/dto/BoardDTO.java
@@ -0,0 +1,60 @@
+package dto;
+
+import java.util.Date;
+
+public class BoardDTO {
+
+	private Long bno;
+	private String author; // writer
+	private String title;
+	private String content;
+	private Long hit;
+	private String ip;
+	private Date postdate;
+	
+	public Long getBno() {
+		return bno;
+	}
+	public void setBno(Long bno) {
+		this.bno = bno;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Long getHit() {
+		return hit;
+	}
+	public void setHit(Long hit) {
+		this.hit = hit;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+	public Date getPostdate() {
+		return postdate;
+	}
+	public void setPostdate(Date postdate) {
+		this.postdate = postdate;
+	}
+	
+	
+	
+}
